1380fbfbdc86a9620f20d93b25136aaf4c210fc1
limaantonio <limaantoniocarlos20@gmail.com>
2020-12-17 20:58:26 -0300

    fix: cors acces control

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use((_, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+	res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
 	res.setHeader('Access-Control-Allow-Credentials', true);
 
 	next();
