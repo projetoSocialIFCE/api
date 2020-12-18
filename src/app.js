56bd00e8751d290da4b02a8e275741a6bc4260a7
limaantonio <limaantoniocarlos20@gmail.com>
2020-12-17 21:08:04 -0300

    fix: cors bodyParser

--- src/app.js
+++ src/app.js
@@ -6,10 +6,12 @@ const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
+
+
 app.use(cors({
 	origin: '*'
 }));
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
